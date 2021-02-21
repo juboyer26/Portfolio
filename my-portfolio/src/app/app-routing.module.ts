bcde238ece2dee8fe297e0a2686509d922bf4a64
Desirebrady <animeart34@gmail.com>
2021-02-21 11:06:58 +0200

    todo list begin

--- my-portfolio/src/app/app-routing.module.ts
+++ my-portfolio/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './Site/home/home.component';
+import { TodoComponent } from './Todo/todo.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'Todo-list', component: TodoComponent },
 ];
 
 @NgModule({
